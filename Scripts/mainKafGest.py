20093918e60dd0b452162fe0f3012347ed523741
MarcoKero <54058077+MarcoKero@users.noreply.github.com>
2022-06-10 08:45:36 +0200

    modalità finale aggiunta

--- Scripts/mainKafGest.py
+++ Scripts/mainKafGest.py
@@ -6,8 +6,8 @@ from kafka import KafkaConsumer, KafkaProducer
 from lib_gest_kaf import read_filejson, pose_thread, wear_thread, prova, decode_json, GestureRecognition
 
 path = "data_config_gestures.json"
-config_info=read_filejson(path)
-datafine=[]
+config_info = read_filejson(path)
+datafine = []
 
 """
 config_inf:
@@ -21,7 +21,7 @@ consumer_wearable_raw_data_name
 consumer_wearable_event_pose_aggreagator_name
 consumer_gesture_recognition_name
 """
-#funziona
+# funziona
 """wearableThread= threading.Thread(target=wear_thread, args=(config_info["consumer_wearable_event_name"],config_info))
 pose_aggregatorThread= threading.Thread(target=pose_thread, args=(config_info["consumer_pose_aggreagator_name"],config_info))
 
@@ -35,77 +35,85 @@ while True:
         print('exit!')
         break"""
 
-#prova()
+# prova()
 
-#metodo2
+# metodo2
 
-consumer = KafkaConsumer(config_info["consumer_wearable_event_name"],config_info["consumer_pose_aggreagator_name"],
-                            bootstrap_servers=config_info["bootstrapservers"],
-                            security_protocol=config_info["security_protocol"],
-                            sasl_mechanism=config_info["sasl_mechanism"],
-                            sasl_plain_username=config_info["sasl_plain_username"],
-                            sasl_plain_password=config_info["sasl_plain_password"])
+consumer = KafkaConsumer(config_info["consumer_wearable_event_name"], config_info["consumer_pose_aggreagator_name"],
+                         bootstrap_servers=config_info["bootstrapservers"],
+                         security_protocol=config_info["security_protocol"],
+                         sasl_mechanism=config_info["sasl_mechanism"],
+                         sasl_plain_username=config_info["sasl_plain_username"],
+                         sasl_plain_password=config_info["sasl_plain_password"])
 
 producer_gesture = KafkaProducer(bootstrap_servers=config_info["bootstrapservers"],
-                                     security_protocol=config_info["security_protocol"],
-                                     sasl_mechanism=config_info["sasl_mechanism"],
-                                     sasl_plain_username=config_info["sasl_plain_username"],
-                                     sasl_plain_password=config_info["sasl_plain_password"])
+                                 security_protocol=config_info["security_protocol"],
+                                 sasl_mechanism=config_info["sasl_mechanism"],
+                                 sasl_plain_username=config_info["sasl_plain_username"],
+                                 sasl_plain_password=config_info["sasl_plain_password"])
+
+modalita_debug=True
+if modalita_debug:
+    MAXFRAMES = 20
+else:
+    MAXFRAMES = 100
 
 for msg in consumer:
 
-    #print(msg)
-    if (msg.topic == config_info["consumer_wearable_event_name"]):# check wearable is shaked
+    # print(msg)
+    if (msg.topic == config_info["consumer_wearable_event_name"]):  # check wearable is shaked
         wear_data = decode_json(msg)
 
         if wear_data["is_shaking"]:
             stringa_json = "shaking device: " + str(wear_data["id"]), " timestamp: " + str(wear_data["timestamp"])
             print(stringa_json)
-            result=GestureRecognition(wear_data["timestamp"],datafine)
-            j=0
+            result = GestureRecognition(wear_data["timestamp"], datafine)
+            j = 0
             for elem in result:
-                if elem==1:
-                    #print(result[j])
-                    print("person "+ str(j)+ " raised arm")
-                    senddata= {}
-                    senddata[config_info["gesture_aggregator_body_id"]] = j
-                    senddata[config_info["gesture_timestamp"]] = wear_data["timestamp"]
-                    senddata[config_info["gesture_wear_id"]] = wear_data["id"]
-                    senddata[config_info["gesture_name"]] = "Raised right hand"
+                if elem == 1:
+                    # print(result[j])
+                    print("person " + str(j) + " raised arm")
+                    senddata = {
+                        config_info["gesture_aggregator_body_id"]: j,
+                        config_info["gesture_timestamp"]: wear_data["timestamp"],
+                        config_info["gesture_wear_id"]: wear_data["id"],
+                        config_info["gesture_name"]: "Raised right hand"
+                    }
                     sendKafka = json.dumps(senddata)
 
                     future = producer_gesture.send("opera_data_gesture_recognition", sendKafka.encode())
                     print("done")
 
-                j=j+1
+                j = j + 1
             print(result)
     else:
-        #msg = read_filejson("GestureRecKafkaDocker/data/befine_no_collision_1_zed/1.json")
-        #print(msg)
+        # msg = read_filejson("GestureRecKafkaDocker/data/befine_no_collision_1_zed/1.json")
+        # print(msg)
         datasample = decode_json(msg)
 
         print("done")
-        #datafine = [datasample, datasample, datasample]
-        #print(datasample)
-        #for msg in consumer:
-            #print("befine")
-            #print(msg)
-            # {"timestamp": 1654607356927.583, "body": [{"body_id": 0, "event": [], "keypoints": {"nose": {"x": NaN, "y": NaN, "z": NaN}, "left_ear": {"x": NaN, "y": NaN, "z": NaN}, "right_ear": {"x": NaN, "y": NaN, "z": NaN}, "left_shoulder": {"x": NaN, "y": NaN, "z": NaN}, "right_shoulder": {"x": NaN, "y": NaN, "z": NaN}, "left_elbow": {"x": NaN, "y": NaN, "z": NaN}, "right_elbow": {"x": NaN, "y": NaN, "z": NaN}, "left_wrist": {"x": NaN, "y": NaN, "z": NaN}, "right_wrist": {"x": NaN, "y": NaN, "z": NaN}, "left_hip": {"x": 10.851959228515625, "y": -4.4818196296691895, "z": -0.900795578956604}, "right_hip": {"x": 10.782848358154297, "y": -4.492856979370117, "z": -0.9190366268157959}, "left_knee": {"x": NaN, "y": NaN, "z": NaN}, "right_knee": {"x": 10.482959747314453, "y": -4.294037342071533, "z": -0.881792426109314}, "left_ankle": {"x": NaN, "y": NaN, "z": NaN}, "right_ankle": {"x": NaN, "y": NaN, "z": NaN}, "neck": {"x": NaN, "y": NaN, "z": NaN}, "chest": {"x": NaN, "y": NaN, "z": NaN}, "mid_hip": {"x": 10.817403793334961, "y": -4.487338066101074, "z": -0.9099161028862}}}]}
-        if len(datafine) >= 20:
+        # datafine = [datasample, datasample, datasample]
+        # print(datasample)
+        # for msg in consumer:
+        # print("befine")
+        # print(msg)
+        # {"timestamp": 1654607356927.583, "body": [{"body_id": 0, "event": [], "keypoints": {"nose": {"x": NaN, "y": NaN, "z": NaN}, "left_ear": {"x": NaN, "y": NaN, "z": NaN}, "right_ear": {"x": NaN, "y": NaN, "z": NaN}, "left_shoulder": {"x": NaN, "y": NaN, "z": NaN}, "right_shoulder": {"x": NaN, "y": NaN, "z": NaN}, "left_elbow": {"x": NaN, "y": NaN, "z": NaN}, "right_elbow": {"x": NaN, "y": NaN, "z": NaN}, "left_wrist": {"x": NaN, "y": NaN, "z": NaN}, "right_wrist": {"x": NaN, "y": NaN, "z": NaN}, "left_hip": {"x": 10.851959228515625, "y": -4.4818196296691895, "z": -0.900795578956604}, "right_hip": {"x": 10.782848358154297, "y": -4.492856979370117, "z": -0.9190366268157959}, "left_knee": {"x": NaN, "y": NaN, "z": NaN}, "right_knee": {"x": 10.482959747314453, "y": -4.294037342071533, "z": -0.881792426109314}, "left_ankle": {"x": NaN, "y": NaN, "z": NaN}, "right_ankle": {"x": NaN, "y": NaN, "z": NaN}, "neck": {"x": NaN, "y": NaN, "z": NaN}, "chest": {"x": NaN, "y": NaN, "z": NaN}, "mid_hip": {"x": 10.817403793334961, "y": -4.487338066101074, "z": -0.9099161028862}}}]}
+        if len(datafine) >= MAXFRAMES:
             datafine.pop(0)
-            result = GestureRecognition(1654607356927.583, datafine)
-            #print(result)
-            j=0
-            for elem in result:
-                if elem==1:
-                    #print(result[j])
-                    print("person "+ str(j)+ " raised arm")
-                    senddata=datafine
-                    senddata["BodyId"] = j
-                    datajson = json.dumps(senddata)
+            if modalita_debug:
+                result = GestureRecognition(1654607356927.583, datafine)
+                # print(result)
+                j = 0
+                for elem in result:
+                    if elem == 1:
+                        # print(result[j])
+                        print("person " + str(j) + " raised arm")
+                        senddata = datafine
+                        senddata["BodyId"] = j
+                        datajson = json.dumps(senddata)
 
-                    future = producer_gesture.send("opera_data_gesture_recognition", datajson.encode())
-                    print("done")
+                        future = producer_gesture.send("opera_data_gesture_recognition", datajson.encode())
+                        print("done")
 
-                j=j+1
+                    j = j + 1
         datafine.append(datasample)
